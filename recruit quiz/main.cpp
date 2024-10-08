30320ebfa57c334cfccd12d6d548bfcaa3820ae4
慶次郎 別田 <keijirou20041010@gmail.com>
2024-10-08 18:46:18 +0900

    問題の数値をランダムに選ぶ。

--- recruit quiz/main.cpp
+++ recruit quiz/main.cpp
@@ -1,35 +1,55 @@
 #include<iostream>
 #include<string>
+#include<random>
 using namespace std;
 
-//問題文と答えをまとめる構造体
+// 問題文と答えをまとめる構造体
 struct Question
 {
-	string s; //問題文
-	int a;    //答え
+	string q; // 問題文
+	int a;    // 答え
 };
 
 int main() {
-	Question questions[] = {
-		{"13*(-5)",13*-5},
-		{"(-21)/(-3)", -21 / 3},
-		{"7-(4+2)/2", 7 - (4 + 2) / 2},
-	};
+	Question questions[3]; // 3つの問題を用意する
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	// 掛け算
+	int x = uniform_int_distribution<>(1, 30)(rand);
+	int y = uniform_int_distribution<>(1, 20)(rand);
+	questions[0].q = to_string(x) + " × " + to_string(y); // 問題文を作成
+	questions[0].a = x * y; // 答えを計算
+
+	// 割り算
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	questions[1].q = to_string(x) + " ÷ " + to_string(y); // 問題文を作成
+	questions[1].a = x / y; // 答えを計算
+
+	// 複雑な式
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + " - (" + to_string(y) + " + " + to_string(z) + ") ÷ " + to_string(w); // 問題文を作成
+	questions[2].a = x - (y + z) / w; // 答えを計算
 
 	cout << "[リクルート試験対策クイズ]\n";
-	
-	for (const auto& e : questions) {
-		cout << e.s << "の答えは?\n";
 
+	for (const auto& e : questions) {
+		cout << e.q << " の答えは?\n"; // 問題文を表示
 
 		int answer;
-		cin >> answer;
-		if (answer == e.a)
-		{
-			cout << "正解" << endl;
+		cin >> answer; // ユーザーからの入力を受け取る
+		if (answer == e.a) {
+			cout << "正解！" << endl;
 		}
 		else {
-			cout << "間違い！正解は" <<e.s << endl;
+			cout << "間違い！ 正解は " << e.a << " です。" << endl;
 		}
-	} //for questions
+	}
+
+	return 0;
 }
