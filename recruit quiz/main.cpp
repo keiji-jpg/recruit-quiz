ed6446e6b0da2e847fba81bd5bf4ceae3d13b47a
慶次郎 別田 <keijirou20041010@gmail.com>
2024-10-15 18:20:22 +0900

    球の体積を求める問題を追加

--- recruit quiz/main.cpp
+++ recruit quiz/main.cpp
@@ -52,6 +52,12 @@ int main() {
 		"底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n"+
 		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",x * x * y / 3 });
 
+	// 球の体積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	questions.push_back({
+		"半径" + to_string(x) + "cmの球がある。\n" +
+		"この球の体積をXπcm^3とする。Xの値を求めよ。", 4 * x * x * x / 3 });
+	
 
 	cout << "[リクルート試験対策クイズ]\n";
 
