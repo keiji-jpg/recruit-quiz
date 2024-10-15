6a40c5589329b31b60316b04dcf58b6f01847a15
慶次郎 別田 <keijirou20041010@gmail.com>
2024-10-15 18:32:24 +0900

    答えをstring型に

--- recruit quiz/main.cpp
+++ recruit quiz/main.cpp
@@ -7,72 +7,71 @@ using namespace std;
 // 問題文と答えをまとめる構造体
 struct Question
 {
-	string q; // 問題文
-	int a;    // 答え
+    string q; // 問題文
+    string a; // 答え
 };
 
 int main() {
-	vector<Question> questions(3); // 3つの問題を用意する
+    vector<Question> questions(3); // 3つの問題を用意する
 
-	random_device rd;
-	mt19937 rand(rd());
+    random_device rd;
+    mt19937 rand(rd());
 
-	// 掛け算
-	int x = uniform_int_distribution<>(1, 30)(rand);
-	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].q = to_string(x) + " × " + to_string(y) + "の答えは?"; // 問題文を作成
-	questions[0].a = x * y; // 答えを計算
+    // 掛け算
+    int x = uniform_int_distribution<>(1, 30)(rand);
+    int y = uniform_int_distribution<>(1, 20)(rand);
+    questions[0].q = to_string(x) + " × " + to_string(y) + "の答えは?"; // 問題文を作成
+    questions[0].a = to_string(x * y); // 答えを計算
 
-	// 割り算
-	x = uniform_int_distribution<>(1, 100)(rand);
-	y = uniform_int_distribution<>(1, 10)(rand);
-	questions[1].q = to_string(x * y) + " ÷ " + to_string(y) + "の答えは?"; // 問題文を作成
-	questions[1].a = x; // 答えを計算
+    // 割り算
+    x = uniform_int_distribution<>(1, 100)(rand);
+    y = uniform_int_distribution<>(1, 10)(rand);
+    questions[1].q = to_string(x * y) + " ÷ " + to_string(y) + "の答えは?"; // 問題文を作成
+    questions[1].a = to_string(x); // 答えを計算
 
-	// 複雑な式
-	x = uniform_int_distribution<>(1, 100)(rand);
-	y = uniform_int_distribution<>(1, 10)(rand);
-	int z = uniform_int_distribution<>(1, 10)(rand);
-	int w = uniform_int_distribution<>(1, 10)(rand);
-	questions[2].q = 
-		to_string(x) + " - (" + to_string(y * w) + " + " + to_string(z * w) + ") ÷ " + to_string(w) + "の答えは?";; // 問題文を作成
-	questions[2].a = x - (y + z); // 答えを計算
+    // 複雑な式
+    x = uniform_int_distribution<>(1, 100)(rand);
+    y = uniform_int_distribution<>(1, 10)(rand);
+    int z = uniform_int_distribution<>(1, 10)(rand);
+    int w = uniform_int_distribution<>(1, 10)(rand);
+    questions[2].q =
+        to_string(x) + " - (" + to_string(y * w) + " + " + to_string(z * w) + ") ÷ " + to_string(w) + "の答えは?"; // 問題文を作成
+    questions[2].a = to_string(x - (y + z)); // 答えを計算
 
-	//三角形の面積
-	x = uniform_int_distribution<>(1, 10)(rand);	
-	y = uniform_int_distribution<>(1, 5)(rand)*2;
-	questions.push_back({
-		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",
-		x });
+    // 三角形の面積
+    x = uniform_int_distribution<>(1, 10)(rand);
+    y = uniform_int_distribution<>(1, 5)(rand) * 2;
+    questions.push_back({
+        "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",
+        to_string(x) });
 
-	//円錐の体積
-	x = uniform_int_distribution<>(1, 10)(rand);
-	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({
-		"底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n"+
-		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",x * x * y / 3 });
+    // 円錐の体積
+    x = uniform_int_distribution<>(1, 10)(rand);
+    y = uniform_int_distribution<>(1, 5)(rand) * 3;
+    questions.push_back({
+        "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" +
+        "この円錐の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * y / 3) });
 
-	// 球の体積
-	x = uniform_int_distribution<>(1, 10)(rand);
-	questions.push_back({
-		"半径" + to_string(x) + "cmの球がある。\n" +
-		"この球の体積をXπcm^3とする。Xの値を求めよ。", 4 * x * x * x / 3 });
-	
+    // 球の体積
+    x = uniform_int_distribution<>(1, 10)(rand);
+    questions.push_back({
+        "半径" + to_string(x) + "cmの球がある。\n" +
+        "この球の体積をXπcm^3とする。Xの値を求めよ。", to_string(4 * x * x * x / 3) });
 
-	cout << "[リクルート試験対策クイズ]\n";
+    cout << "[リクルート試験対策クイズ]\n";
 
-	for (const auto& e : questions) {
-		cout << e.q << " \n"; // 問題文を表示
+    for (const auto& e : questions) {
+        cout << e.q << " \n"; // 問題文を表示
 
-		int answer;
-		cin >> answer; // ユーザーからの入力を受け取る
-		if (answer == e.a) {
-			cout << "正解！" << endl;
-		}
-		else {
-			cout << "間違い！ 正解は " << e.a << " です。" << endl;
-		}
-	}
+        string answer;
+        cin >> answer; // ユーザーからの入力を受け取る
+        if (answer == e.a) {
+            cout << "正解！" << endl;
+        }
+        else {
+            cout << "間違い！ 正解は " << e.a << " です。" << endl;
+        }
+    }
 
-	return 0;
+    return 0;
 }
