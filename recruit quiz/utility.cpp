35e7169051467a520fe3eee2e9fce55ba2997f24
慶次郎 別田 <keijirou20041010@gmail.com>
2024-10-22 19:08:02 +0900

    熟語を選ぶ問題を追加

--- recruit quiz/utility.cpp
+++ recruit quiz/utility.cpp
@@ -11,14 +11,45 @@ vector<int>CreateRandomIndices(int n)
 		indices[i] = i;
 	}
 	//番号の配列のシャッフル
-	random_device rd;
-	mt19937 rand(rd());
-	for (int i = n - 1; i > 0; i--) {
-		const int j = uniform_int_distribution<>(0, i)(rand);
-		const int tmp = indices[i];
-		indices[i] = indices[j];
-		indices[j] = tmp;
+
+	Shuffle(indices);
+	return indices;
+}
+	void Shuffle(vector<int>&indices)
+	{
+		const int n = static_cast<int>(indices.size());
+
+
+		random_device rd;
+		mt19937 rand(rd());
+		for (int i = n - 1; i > 0; i--) {
+			const int j = uniform_int_distribution<>(0, i)(rand);
+			const int tmp = indices[i];
+			indices[i] = indices[j];
+			indices[j] = tmp;
+		}
 	}
+	
+
+
+//間違った番号の配列を作成する
+
+vector<int>CreateWrongIndices(int n, int correctIndex)
+{
+	//番号を配列に格納
+	vector<int>indices(n - 1);
+
+	for (int i = 0; i < correctIndex; i++) {
+		indices[i] = i;
+	}
+	for (int i = correctIndex; i < n - 1; i++)
+	{
+		indices[i] = i + 1;
+
+	}
+
+	//番号を配列をシャッフル
+	Shuffle(indices);
 
 	return indices;
 }
