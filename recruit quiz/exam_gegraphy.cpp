28c12e6b9e4382b5facd16e874b46e0aea432853
慶次郎 別田 <keijirou20041010@gmail.com>
2024-12-10 18:53:29 +0900

    新しい問題を追加

--- recruit quiz/exam_gegraphy.cpp
+++ recruit quiz/exam_gegraphy.cpp
@@ -46,7 +46,7 @@ QuestionList CreatePrefecturesExam()
 	random_device rd;
 
 	/*const int type = uniform_int_distribution<>(0, 2)(rd);*/
-	const int type = 0;
+	const int type = 1;
 	switch (type)
 	{
 	case 0: //特徴から都道府県を答える
@@ -84,6 +84,31 @@ QuestionList CreatePrefecturesExam()
 		}
 		break;
 	case 1: //都道府県をあらわす特徴を答える
+		for (int i = 0; i < quizCount; i++)
+		{
+			//正解以外の答えをランダムに選ぶ
+			const int correctIndex = indices[i];
+			vector<int> answer = CreateWrongIndices((int)data.size(), correctIndex);
+
+
+
+			//ランダムな位置を正しい番号で上書き
+			const int correctNo = std::uniform_int_distribution<>(1, 3)(rd);
+			answer[correctNo - 1] = correctIndex;
+
+			//問題を作成
+			string s = "「" + data[correctIndex].name + "」の特徴をあらわす番号を選べ";
+			//答えとなる都道府県を問題文に追加
+			for (int j = 0; j < 3; j++)
+			{
+				const auto& features = data[answer[j]].features;
+				const int n = uniform_int_distribution<>(0, (int)features.size() - 1)(rd);
+				s += "\n" + to_string(j + 1) + ":" + features[n];
+			}
+			//問題文と答えのペアを問題配列に追加
+			questions.push_back({ s,to_string(correctNo) });
+		}
+
 		break;
 	case 2: //都道府県から県庁所在地を答える
 		break;
